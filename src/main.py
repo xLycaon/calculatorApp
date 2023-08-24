714faa9e38355d89da02eab2545f3d68e23cc208
xLycaon <lycaon@DESKTOP-0G05GOO>
2023-08-24 13:29:20 +0300

    Placed the buttons in their correct positions and resized the window

--- src/main.py
+++ src/main.py
@@ -1,43 +1,53 @@
 import tkinter as tk
 
 
-
-#TODO: place the buttons in the correct positions
 #TODO: add functionality to the buttons
-#TODO: the size of the text in the text box should be the same height as the text box
 #TODO: string analyzer function
 #TODO: transform this program into an executable file
 
+input_expression =""
 
 def setup():
     #create the Window
     window = tk.Tk()
-    window.geometry('650x300')
+    window.geometry('648x360')
     window.title('Calculator')
+    window.resizable(0,0)   
 
 
-    #Create the buttons and text box
-    text_box_1 = tk.Text(window,width=80,height=5)
-    button_0 = tk.Button(window,text='0',width=30,command=lambda: text_box_1.insert(tk.END,'0'))
-    button_1 = tk.Button(window,text='1',width=15,command=lambda: text_box_1.insert(tk.END,'1'))
-    button_2 = tk.Button(window,text='2',width=15,command=lambda: text_box_1.insert(tk.END,'2'))
-    button_3 = tk.Button(window,text='3',width=15,command=lambda: text_box_1.insert(tk.END,'3'))
-    button_4 = tk.Button(window,text='4',width=15,command=lambda: text_box_1.insert(tk.END,'4'))
-    button_5 = tk.Button(window,text='5',width=15,command=lambda: text_box_1.insert(tk.END,'5'))
-    button_6 = tk.Button(window,text='6',width=15,command=lambda: text_box_1.insert(tk.END,'6'))
-    button_7 = tk.Button(window,text='7',width=15,command=lambda: text_box_1.insert(tk.END,'7'))
-    button_8 = tk.Button(window,text='8',width=15,command=lambda: text_box_1.insert(tk.END,'8'))
-    button_9 = tk.Button(window,text='9',width=15,command=lambda: text_box_1.insert(tk.END,'9'))
-    button_add = tk.Button(window,text='+',width=15,command=lambda: text_box_1.insert(tk.END,'+'))
-    button_sub = tk.Button(window,text='-',width=15,command=lambda: text_box_1.insert(tk.END,'-'))
-    button_eq = tk.Button(window,text='=',width=15,command=lambda: text_box_1.insert(tk.END,'='))
-    button_mul = tk.Button(window,text='*',width=15,command=lambda: text_box_1.insert(tk.END,'*'))
-    button_div = tk.Button(window,text='/',width=15,command=lambda: text_box_1.insert(tk.END,'/'))
-    button_per = tk.Button(window,text='%',width=15,command=lambda: text_box_1.insert(tk.END,'%'))
-    button_del = tk.Button(window,text='del',width=15,command=lambda: text_box_1.delete(len(text_box_1.get())-1))
-    clear_button = tk.Button(window,text='Clear',width=15,command=lambda: text_box_1.delete(0,tk.END))
+    #-----Create the buttons and text box--------
 
-    
+    #create the frame for the input field
+    frame_txt = tk.Frame(window,highlightbackground='black',highlightthickness=2,bd=0,width=648)
+    frame_txt.grid(row=0,column=0,ipadx=13)
+    text_box_1 = tk.Entry(frame_txt,width=36,justify='right',textvariable=input_expression,font=('Arial',18,'bold'))
+    text_box_1.pack(expand=True,fill='both',side='left',ipadx=0,ipady=10)
+
+    #create the frame for the buttons
+    frame_btn = tk.Frame(window, width=648, bg="black",highlightbackground='black',highlightthickness=2,bd=0)
+    frame_btn.grid(row=1,column=0)
+
+    #create the buttons
+    button_0 = tk.Button(frame_btn,text='0',width=33,height=2,command=lambda: text_box_1.insert(tk.END,'0'),padx=10,pady=10)
+    button_1 = tk.Button(frame_btn,text='1',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'1'),padx=10,pady=10)
+    button_2 = tk.Button(frame_btn,text='2',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'2'),padx=10,pady=10)
+    button_3 = tk.Button(frame_btn,text='3',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'3'),padx=10,pady=10)
+    button_4 = tk.Button(frame_btn,text='4',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'4'),padx=10,pady=10)
+    button_5 = tk.Button(frame_btn,text='5',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'5'),padx=10,pady=10)
+    button_6 = tk.Button(frame_btn,text='6',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'6'),padx=10,pady=10)
+    button_7 = tk.Button(frame_btn,text='7',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'7'),padx=10,pady=10)
+    button_8 = tk.Button(frame_btn,text='8',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'8'),padx=10,pady=10)
+    button_9 = tk.Button(frame_btn,text='9',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'9'),padx=10,pady=10)
+    button_add = tk.Button(frame_btn,text='+',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'+'),padx=10,pady=10)
+    button_sub = tk.Button(frame_btn,text='-',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'-'),padx=10,pady=10)
+    button_eq = tk.Button(frame_btn,text='=',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'='),padx=10,pady=10)
+    button_mul = tk.Button(frame_btn,text='*',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'*'),padx=10,pady=10)
+    button_div = tk.Button(frame_btn,text='/',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'/'),padx=10,pady=10)
+    button_per = tk.Button(frame_btn,text='%',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'%'),padx=10,pady=10)
+    button_del = tk.Button(frame_btn,text='del',width=15,height=2,command=lambda: text_box_1.delete(len(text_box_1.get())-1),padx=10,pady=10)
+    clear_button = tk.Button(frame_btn,text='Clear',width=15,height=2,command=lambda: text_box_1.delete(0,tk.END),padx=10,pady=10)
+    dot_button = tk.Button(frame_btn,text='.',width=15,height=2,command=lambda: text_box_1.insert(tk.END,'.'),padx=10,pady=10)
+
     #------------------ Layout ------------------
     button_0.grid(row=5,column=0,columnspan=2)
     button_1.grid(row=4,column=0)
@@ -56,8 +66,8 @@ def setup():
     button_div.grid(row=1,column=3)
     button_per.grid(row=1,column=1)
     button_del.grid(row=1,column=0)
-    clear_button.grid(row=6,column=1)
-    text_box_1.grid(row=0,column=0,columnspan=4)
+    clear_button.grid(row=5,column=3)
+    dot_button.grid(row=5,column=2)
     #------------------ Layout ------------------
 
 
